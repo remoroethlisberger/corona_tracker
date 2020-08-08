fbb1189c28e826ff0b3c187a7b18dc7cd271f816
remoroethlisberger <remo.roethlisberger@students.unibe.ch>
2020-08-08 21:41:33 +0200

    Deploy update

--- src/scripts/deploy.sh
+++ src/scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -a
-source ./.env
+source ../../.env
 set +a
 /usr/local/bin/npm run build
 ftp -n $DEPLOY_HOST <<SCRIPT
 user $USER $PASSWD
 cd web/corona_tracker/
-lcd build
+lcd ../../build
 mput *
 a
 cd static/css
