8885e1eba144c57911b7d9aa79d7f16144758afe
remoroethlisberger <remo.roethlisberger@students.unibe.ch>
2020-08-10 01:03:18 +0200

    Adding french

--- src/elements/PageWrapper/PageWrapper.js
+++ src/elements/PageWrapper/PageWrapper.js
@@ -5,10 +5,12 @@ import './PageWrapper.css';
 import Navbar from '../Navbar/Navbar';
 
 import ReactGA from 'react-ga';
+import { useTranslation } from 'react-i18next';
 ReactGA.initialize('UA-174993755-1');
 
 const PageWrapper = (props) => {
   ReactGA.pageview(window.location.pathname + window.location.hash);
+  const { t, i18n } = useTranslation();
   return (
     <div className="container p-0">
       <Navbar {...props} />
@@ -17,6 +19,21 @@ const PageWrapper = (props) => {
         <div className="d-flex flex-row justify-content-around">
           <div className="small">
             <a href="https://waremama.ch">waremama.ch</a>
+          </div>
+          <div className="small">
+            <a href="" onClick={() => i18n.changeLanguage('de')}>
+              DE
+            </a>
+          </div>
+          <div className="small">
+            <a href="" onClick={() => i18n.changeLanguage('en')}>
+              EN
+            </a>
+          </div>
+          <div className="small">
+            <a href="" onClick={() => i18n.changeLanguage('fr')}>
+              FR
+            </a>
           </div>
           <p className="small">Remo Röthlisberger, 2020</p>
         </div>
