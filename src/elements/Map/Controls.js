81c2fca6aa57f685ad27ee4439bc127bf61a585c
remoroethlisberger <remo.r@gmx.ch>
2020-10-11 22:44:05 +0200

    Adding play/pause on space

--- src/elements/Map/Controls.js
+++ src/elements/Map/Controls.js
@@ -11,12 +11,27 @@ import {
 
 import { useTranslation } from 'react-i18next';
 import { map } from 'd3';
+import { useEffect } from 'react';
 
 const Controls = (props) => {
   const [mapState, dispatch] = useMapContext();
   const { t } = useTranslation();
   const OVER_THE_LAST = t('over_the_last');
   const DAYS = t('days');
+
+  useEffect(() => {
+    const playOnEnter = (e) => {
+      e.preventDefault();
+      if (e.keyCode == 32) {
+        dispatch({ type: mapState.play ? 'pause' : 'play' })
+      }
+    }
+
+    document.addEventListener('keydown', playOnEnter)
+    return () => {
+      document.removeEventListener('keydown', playOnEnter)
+    }
+  }, [mapState.play])
 
   return (
     <div>
@@ -60,22 +75,22 @@ const Controls = (props) => {
           {window.innerWidth <= 800 ? (
             <></>
           ) : (
-            <div style={{ width: '200px', float: 'right' }}>
-              <select
-                className="custom-select"
-                value={mapState.n}
-                onChange={(e) =>
-                  dispatch({ type: 'changeN', value: e.target.value })
-                }
-              >
-                <option value="15">{OVER_THE_LAST + ' 15 ' + DAYS}</option>
-                <option value="10">{OVER_THE_LAST + ' 10 ' + DAYS}</option>
-                <option value="5">{OVER_THE_LAST + ' 5 ' + DAYS}</option>
-                <option value="3">{OVER_THE_LAST + ' 3 ' + DAYS}</option>
-                <option value="1">{t('over_the_last_1')}</option>
-              </select>
-            </div>
-          )}
+              <div style={{ width: '200px', float: 'right' }}>
+                <select
+                  className="custom-select"
+                  value={mapState.n}
+                  onChange={(e) =>
+                    dispatch({ type: 'changeN', value: e.target.value })
+                  }
+                >
+                  <option value="15">{OVER_THE_LAST + ' 15 ' + DAYS}</option>
+                  <option value="10">{OVER_THE_LAST + ' 10 ' + DAYS}</option>
+                  <option value="5">{OVER_THE_LAST + ' 5 ' + DAYS}</option>
+                  <option value="3">{OVER_THE_LAST + ' 3 ' + DAYS}</option>
+                  <option value="1">{t('over_the_last_1')}</option>
+                </select>
+              </div>
+            )}
         </div>
         <button
           className="btn btn-primary"
@@ -88,22 +103,22 @@ const Controls = (props) => {
       {window.innerWidth > 800 ? (
         <></>
       ) : (
-        <div className="m-auto pt-2" style={{ width: '80%' }}>
-          <select
-            className="custom-select"
-            value={mapState.n}
-            onChange={(e) =>
-              dispatch({ type: 'changeN', value: e.target.value })
-            }
-          >
-            <option value="15">{OVER_THE_LAST + ' 15 ' + DAYS}</option>
-            <option value="10">{OVER_THE_LAST + ' 10 ' + DAYS}</option>
-            <option value="5">{OVER_THE_LAST + ' 5 ' + DAYS}</option>
-            <option value="3">{OVER_THE_LAST + ' 3 ' + DAYS}</option>
-            <option value="1">{t('over_the_last_1')}</option>
-          </select>
-        </div>
-      )}
+          <div className="m-auto pt-2" style={{ width: '80%' }}>
+            <select
+              className="custom-select"
+              value={mapState.n}
+              onChange={(e) =>
+                dispatch({ type: 'changeN', value: e.target.value })
+              }
+            >
+              <option value="15">{OVER_THE_LAST + ' 15 ' + DAYS}</option>
+              <option value="10">{OVER_THE_LAST + ' 10 ' + DAYS}</option>
+              <option value="5">{OVER_THE_LAST + ' 5 ' + DAYS}</option>
+              <option value="3">{OVER_THE_LAST + ' 3 ' + DAYS}</option>
+              <option value="1">{t('over_the_last_1')}</option>
+            </select>
+          </div>
+        )}
     </div>
   );
 };
